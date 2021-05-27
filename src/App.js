a1c898f73bd77f8bf27dddde8ede55f27c4822ac
Rutwik1801 <65919321+Rutwik1801@users.noreply.github.com>
2021-05-27 18:02:00 +0530

    Add files via upload
    converted the same context api clone to redux without the drag and drop in working condition.

--- src/App.js
+++ src/App.js
@@ -1,134 +1,85 @@
-import { SettingsInputAntenna } from '@material-ui/icons';
-import React, { Component }  from 'react';
-import {useState} from 'react';
+import React from 'react';
 import { DragDropContext } from 'react-beautiful-dnd';
-import {v4 as uuid} from 'uuid';
-// import logo from './logo.svg';
+import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
 import InputContainer from './components/Input/InputContainer';
 import List from "./components/list/List";
-import store from "./utils/store";
-import StoreAPI from './utils/storeAPI';
+import { dataSliceActions } from './store/data-slice';
+import store from "./store/index";
+
 function App() {
-  const [data,setData]=useState(store);
-  // ========================
-  // ============================
-  const addMoreCard=(title,listId,cardDesignation)=>{
-    const newCardId=uuid();
-    const newCard={
-      id:newCardId,
-      title,
-      designation:cardDesignation
+
+  const items = useSelector((state) => state.allData.data)
+  // // ========================================
+  // // Function for drag n drop
+  // // ============================================
+
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+    if (!destination) {
+      return;
     }
-    const list=data.lists[listId];
-    list.cards=[...list.cards,newCard];
+    const sourceList = items.lists[source.droppableId];
+    const destinationList = items.lists[destination.droppableId];
+    const draggingCard = sourceList.cards.filter((card) =>
+      card.id === draggableId
+    )[0];
+    if (source.droppableId === destination.droppableId) {
+      sourceList.cards.splice(source.index, 1);
+      destinationList.cards.splice(destination.index, 0, draggingCard);
 
-    const newState={
-      ...data,
-      lists:{
-        ...data.lists,
-        [listId]:list
-      }
+      // useDispatch(dataSliceActions.changePosition({
+      //     id:sourceList.id,
+      //     destinationList:destinationList
+      // }))
+      // const newState = {
+      //   ...data,
+      //   lists: {
+      //     ...data.lists,
+      //     [sourceList.id]: destinationList
+      //   }
+      // }
+      // setData(newState);
     }
-    setData(newState);
-    console.log(newCardId);
-    // console.log(title,listId);
-  };
-
-  // ======================
-  // ===================
-
-//  ========================
-// ===========================
-const addMoreList=(title)=>{
-  const newListId=uuid();
-const newList={
-  id:newListId,
-  title,
-  cards:[]
-}
-const newState={
-  listIds:[...data.listIds,newListId],
-  lists:{
-    ...data.lists,
-    [newListId]:newList
-  }
-}
-setData(newState);
-}
-// ==============================
-// ============================
-
-// ======================
-// ======================
-const updateListTitle=(title,listId)=>{
-  const list=data.lists[listId];
-  list.title=title;
-  const newState={
-    ...data,
-    lists:{
-      ...data.lists,
-      [listId]:list
+    else {
+      sourceList.cards.splice(source.index, 1);
+      destinationList.cards.splice(destination.index, 0, draggingCard);
+      //   useDispatch(dataSliceActions.changePositionList({
+      //     sourceId:sourceList.id,
+      //     sourceList:sourceList,
+      //     destinationListId:destinationList.id,
+      //     destinationList:destinationList
+      // }))
+      // const newState = {
+      //   ...data,
+      //   lists: {
+      //     ...data.lists,
+      //     [sourceList.id]: sourceList,
+      //     [destinationList.id]: destinationList
+      //   }
+      // }
+      // setData(newState);
     }
   }
-  setData(newState);
-console.log(title);
-}
-// ==================
-// ==================
+  // // ===========================================
+  // // ===========================================
 
-const onDragEnd=(result)=>{
-const {destination,source,draggableId}=result;
-console.log(destination,source,draggableId);
-if(!destination){
-  return;
-}
-const sourceList=data.lists[source.droppableId];
-const destinationList=data.lists[destination.droppableId];
-const draggingCard=sourceList.cards.filter((card)=>
-  card.id===draggableId
-)[0];
-if(source.droppableId===destination.droppableId){
-sourceList.cards.splice(source.index,1);
-destinationList.cards.splice(destination.index,0,draggingCard);
-// console.log(sourceList);
-const newState={
-  ...data,
-  lists:{
-    ...data.lists,
-    [sourceList.id]:destinationList
-  }
-}
-setData(newState);
-} else{
-  sourceList.cards.splice(source.index,1);
-  destinationList.cards.splice(destination.index,0,draggingCard);
-  const newState={
-    ...data,
-    lists:{
-      ...data.lists,
-      [sourceList.id]:sourceList,
-      [destinationList.id]:destinationList
-    }
-  }
-  setData(newState);
-}
-}
 
   return (
-    <StoreAPI.Provider value={{addMoreCard,addMoreList,updateListTitle}}>
+
     <DragDropContext onDragEnd={onDragEnd}>
-    <div className="App">
-    {data.listIds.map((listId) =>{
-      const list=data.lists[listId]
-      return <List list={list} key={listId} />
-    })}
-    <InputContainer type="list" />
-    </div>
+      <div className="App">
+        {items.listIds.map((listId) => {
+          const list = items.lists[listId]
+          return <List list={list} key={listId} />
+        })}
+        <InputContainer type="list" />
+      </div>
     </DragDropContext>
-     </StoreAPI.Provider>
+
 
   );
 }
 
 export default App;
+
